50e77aa03b47255636c3678b62ec5b4e66949c4c
Abdumannonov-Samandar <156022272+Abdumannonov-Samandar@users.noreply.github.com>
2024-08-04 15:32:24 +0500

    menu fix and all items add

---
+++ src/components/all.tsx
@@ -0,0 +1,17 @@
+import Pitsa from './Pitsa'
+import Burger from './Burger'
+import Kombo from './Kombo'
+import Drinks from './Drinks'
+
+const all = () => {
+  return (
+    <>
+    <Pitsa />
+    <Burger />
+    <Kombo />
+    <Drinks />
+    </>
+  )
+}
+
+export default all
